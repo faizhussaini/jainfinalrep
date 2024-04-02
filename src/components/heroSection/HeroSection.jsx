8ae882309e6d32813c48781859da1ba2f4552158
Faiz_Hussaini <107918420+faizhussaini@users.noreply.github.com>
2024-04-02 20:24:19 +0530

    ttt

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
         src="https://images.vexels.com/media/users/3/194732/raw/90137aa80ed4d208bc0cda1fc224cfff-online-shopping-web-slider.jpg"
         alt=""
       />
-      ../img/hero1.png
+      
     </div>
   );
 };
